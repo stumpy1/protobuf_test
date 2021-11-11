58f99febbc46e5c29e6e38ccba896d41d0f1a462
stumpy1 <dev@alpala.uk>
2021-11-11 20:38:25 +0000

    add Greeter service

--- service/service.pb.go
+++ service/service.pb.go
@@ -83,6 +83,53 @@ func (x *Hello) GetInfo() string {
 	return ""
 }
 
+type HelloReply struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Ack string `protobuf:"bytes,1,opt,name=ack,proto3" json:"ack,omitempty"`
+}
+
+func (x *HelloReply) Reset() {
+	*x = HelloReply{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_github_com_stumpy1_protobuf_test_proto_service_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *HelloReply) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*HelloReply) ProtoMessage() {}
+
+func (x *HelloReply) ProtoReflect() protoreflect.Message {
+	mi := &file_github_com_stumpy1_protobuf_test_proto_service_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use HelloReply.ProtoReflect.Descriptor instead.
+func (*HelloReply) Descriptor() ([]byte, []int) {
+	return file_github_com_stumpy1_protobuf_test_proto_service_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *HelloReply) GetAck() string {
+	if x != nil {
+		return x.Ack
+	}
+	return ""
+}
+
 var File_github_com_stumpy1_protobuf_test_proto_service_proto protoreflect.FileDescriptor
 
 var file_github_com_stumpy1_protobuf_test_proto_service_proto_rawDesc = []byte{
@@ -94,10 +141,16 @@ var file_github_com_stumpy1_protobuf_test_proto_service_proto_rawDesc = []byte{
 	0x20, 0x01, 0x28, 0x05, 0x52, 0x02, 0x69, 0x64, 0x12, 0x12, 0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65,
 	0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x12, 0x12, 0x0a, 0x04,
 	0x69, 0x6e, 0x66, 0x6f, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x69, 0x6e, 0x66, 0x6f,
-	0x42, 0x2a, 0x5a, 0x28, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x73,
-	0x74, 0x75, 0x6d, 0x70, 0x79, 0x31, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x5f,
-	0x74, 0x65, 0x73, 0x74, 0x2f, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x62, 0x06, 0x70, 0x72,
-	0x6f, 0x74, 0x6f, 0x33,
+	0x22, 0x1e, 0x0a, 0x0a, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x52, 0x65, 0x70, 0x6c, 0x79, 0x12, 0x10,
+	0x0a, 0x03, 0x61, 0x63, 0x6b, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x03, 0x61, 0x63, 0x6b,
+	0x32, 0x3c, 0x0a, 0x07, 0x47, 0x72, 0x65, 0x65, 0x74, 0x65, 0x72, 0x12, 0x31, 0x0a, 0x08, 0x53,
+	0x61, 0x79, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x12, 0x0e, 0x2e, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63,
+	0x65, 0x2e, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x1a, 0x13, 0x2e, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63,
+	0x65, 0x2e, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x52, 0x65, 0x70, 0x6c, 0x79, 0x22, 0x00, 0x42, 0x2a,
+	0x5a, 0x28, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x73, 0x74, 0x75,
+	0x6d, 0x70, 0x79, 0x31, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x5f, 0x74, 0x65,
+	0x73, 0x74, 0x2f, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74,
+	0x6f, 0x33,
 }
 
 var (
@@ -112,13 +165,16 @@ func file_github_com_stumpy1_protobuf_test_proto_service_proto_rawDescGZIP() []b
 	return file_github_com_stumpy1_protobuf_test_proto_service_proto_rawDescData
 }
 
-var file_github_com_stumpy1_protobuf_test_proto_service_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_github_com_stumpy1_protobuf_test_proto_service_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_github_com_stumpy1_protobuf_test_proto_service_proto_goTypes = []interface{}{
-	(*Hello)(nil), // 0: service.Hello
+	(*Hello)(nil),      // 0: service.Hello
+	(*HelloReply)(nil), // 1: service.HelloReply
 }
 var file_github_com_stumpy1_protobuf_test_proto_service_proto_depIdxs = []int32{
-	0, // [0:0] is the sub-list for method output_type
-	0, // [0:0] is the sub-list for method input_type
+	0, // 0: service.Greeter.SayHello:input_type -> service.Hello
+	1, // 1: service.Greeter.SayHello:output_type -> service.HelloReply
+	1, // [1:2] is the sub-list for method output_type
+	0, // [0:1] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
 	0, // [0:0] is the sub-list for extension extendee
 	0, // [0:0] is the sub-list for field type_name
@@ -142,6 +198,18 @@ func file_github_com_stumpy1_protobuf_test_proto_service_proto_init() {
 				return nil
 			}
 		}
+		file_github_com_stumpy1_protobuf_test_proto_service_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*HelloReply); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -149,9 +217,9 @@ func file_github_com_stumpy1_protobuf_test_proto_service_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_github_com_stumpy1_protobuf_test_proto_service_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   1,
+			NumMessages:   2,
 			NumExtensions: 0,
-			NumServices:   0,
+			NumServices:   1,
 		},
 		GoTypes:           file_github_com_stumpy1_protobuf_test_proto_service_proto_goTypes,
 		DependencyIndexes: file_github_com_stumpy1_protobuf_test_proto_service_proto_depIdxs,
